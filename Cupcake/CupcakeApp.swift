2fb403a7a8c365878ba7271b03679c1cd6c5a29b
Magdalena Struniawska <mag.sok@onet.pl>
2022-04-13 09:55:08 +0200

    Initial Commit

---
+++ Cupcake/CupcakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeApp.swift
+//  Cupcake
+//
+//  Created by Slawek on 13/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
